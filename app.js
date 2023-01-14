e4edaba97d8bf9e01f934db7b4dd646177f8a047
Benjamín Álvarez Rodríguez <fightsthumbs@gmail.com>
2023-01-13 23:13:12 -0600

    logica implementada, un poco más de disenno

--- app.js
+++ app.js
@@ -9,13 +9,16 @@ para.setAttribute("class","firstCasilla");
 
 console.log(para);
 
-textBoxCont.appendChild(para);
+// textBoxCont.appendChild(para);
 
 
 
 const stepNumber = document.getElementById("step-number");
 const isIncremental = document.getElementById("incremental");
+console.log("the value of incremental is" + typeof isIncremental.value);
+console.log(isIncremental.checked);
 console.log(isIncremental);
+console.log(typeof stepNumber.value)
 const acceptButton = document.getElementById("accept");
 acceptButton.addEventListener("click", (e) => {
   while (textBoxCont.hasChildNodes()) {
@@ -25,14 +28,64 @@ acceptButton.addEventListener("click", (e) => {
   console.log(stepNumber.value)
   console.log(isIncremental.value)
   
-  for (let i = 1; i <= stepNumber.value; i++) {
-    let elementitof = document.createElement("input");
-    elementitof.setAttribute("type","number")
-    elementitof.setAttribute("class", "casilla_"+i);
-    textBoxCont.appendChild(elementitof);
+  let caseNum = 1;
+  let multiplier= parseFloat(stepNumber.value);
+  const MULT = parseFloat(stepNumber.value);
+
+  console.log(typeof MULT + typeof multiplier);
+  let number = 1000;
+  for (; number > 0 ; ) {
+    console.log(number)
+    let casillaElem = document.createElement("input");
+    casillaElem.setAttribute("type","number");
+    casillaElem.setAttribute("class", "casilla_"+caseNum);
+    casillaElem.setAttribute("rightanswer",number);
+    caseNum+=1;
+
+    casillaElem.addEventListener("keypress", (e) => {
+      if(e.key === "Enter" || e.keyCode === 32) {
+        // console.log(e.target.nextElementSibling
+        console.log("presionó eneter");
+        e.target.nextElementSibling.focus();
+      }
+
+      
+    })
+
+    casillaElem.addEventListener("change",(x)=>{
+
+        let userInput = x.target.value;
+        let rightAnswer = x.target.attributes.rightanswer.value;
+        if(userInput == rightAnswer) {
+          x.target.setAttribute("class","right-green");
+        } else {
+          console.log("respuesta incorrecta")
+        }
+
+      console.log("Helloman");
+    })
+
+
+    textBoxCont.appendChild(casillaElem);
+
+    if (isIncremental.checked === true) {
+      console.log(multiplier);
+      number -= multiplier;
+      multiplier += MULT;
+    } else {
+      number -= multiplier;
+    }
+
   }
   textBoxCont.firstChild.setAttribute("value",1000);
+  textBoxCont.firstChild.classList.add("right-green");
 
+/*   textBoxCont.children.forEach(element, element => {
+    element.addEventListener("change", () => {
+      console.log("cambio en un elemento");
+    })
+  });
+   */
 
 })
 
